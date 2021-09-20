0fe8b790b3b0ec0badc45de9f76e71e0deac253a
atenyaeves <atenyaev@protonmail.com>
2021-09-20 09:38:52 +0700

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+/**
+ * First Class
+ */
+public class Main {
+    /**
+     * Entry Point
+     * Console output "Hello word"
+     * @param args - arguments array
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+    }
+}
